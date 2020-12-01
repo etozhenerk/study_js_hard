0bcbdd44fd1e5ceac481efb812cb4d1950b49df4
etozhenerk <etozhenerk@yandex.ru>
2020-12-01 21:13:09 +0300

    Lesson 9

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
-'use strict';
+"use strict";
+function getFormatA() {
+  let now = new Date();
+  let hours = now.getHours(),
+    minutes = now.getMinutes(),
+    seconds = now.getSeconds();
+  let options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    weekday: "long",
+  };
+  let element = document.querySelector(".time1");
 
-let week = [
-  "Понедельник",
-  "Вторник",
-  "Среда",
-  "Четверг",
-  "Пятница",
-  "Суббота",
-  "Воскресенье",
-];
-
-let now = new Date().getDay();
-week.forEach(function(item, i){
-  if(i === (now-1)){
-    document.querySelector('.week').innerHTML += `<b>${item}</b><br>`;
-  }else if(i === 5 || i ===6){
-    document.querySelector('.week').innerHTML += `<i>${item}</i><br>`;
-  }else{
-    document.querySelector('.week').innerHTML += `${item}<br>`;
+  let h = "",
+    m = "",
+    s = "";
+  if (hours % 10 === 1) {
+    h = "час";
+  } else if (hours % 10 === 2 || hours % 10 === 3 || hours % 10 === 4) {
+    h = "часа";
+  } else {
+    h = "часов";
   }
+  if (minutes % 10 === 1) {
+    m = "минута";
+  } else if (minutes % 10 === 2 || minutes % 10 === 3 || minutes % 10 === 4) {
+    m = "минуты";
+  } else {
+    m = "минут";
+  }
+  if (seconds % 10 === 1) {
+    s = "секунда";
+  } else if (seconds % 10 === 2 || seconds % 10 === 3 || seconds % 10 === 4) {
+    s = "секунды";
+  } else {
+    s = "секунд";
+  }
+  element.innerHTML = `Сегодня ${now.toLocaleString(
+    "ru",
+    options
+  )} ${hours} ${h} ${minutes} ${m} ${seconds} ${s}`;
+}
+function getFormatB() {
+  let now = new Date();
 
-  
-});
+  let element = document.querySelector(".time2");
+  element.innerHTML = now.toLocaleString("ru") ;
+}
+
+setInterval(getFormatA, 1000);
+setInterval(getFormatB, 1000);
