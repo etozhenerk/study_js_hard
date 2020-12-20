dc1051634c063c071c327e04439aa3b6a60b373b
etozhenerk <etozhenerk@yandex.ru>
2020-12-20 15:13:01 +0300

    Lesson 13

--- script.js
+++ script.js
@@ -1,52 +1,14 @@
 "use strict";
-function getFormatA() {
-  let now = new Date();
-  let hours = now.getHours(),
-    minutes = now.getMinutes(),
-    seconds = now.getSeconds();
-  let options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    weekday: "long",
-  };
-  let element = document.querySelector(".time1");
 
-  let h = "",
-    m = "",
-    s = "";
-  if (hours % 10 === 1) {
-    h = "час";
-  } else if (hours % 10 === 2 || hours % 10 === 3 || hours % 10 === 4) {
-    h = "часа";
-  } else {
-    h = "часов";
-  }
-  if (minutes % 10 === 1) {
-    m = "минута";
-  } else if (minutes % 10 === 2 || minutes % 10 === 3 || minutes % 10 === 4) {
-    m = "минуты";
-  } else {
-    m = "минут";
-  }
-  if (seconds % 10 === 1) {
-    s = "секунда";
-  } else if (seconds % 10 === 2 || seconds % 10 === 3 || seconds % 10 === 4) {
-    s = "секунды";
-  } else {
-    s = "секунд";
-  }
-  element.innerHTML = `Сегодня ${now.toLocaleString(
-    "ru",
-    options
-  )} ${hours} ${h} ${minutes} ${m} ${seconds} ${s}`;
-}
-function getFormatB() {
-  let now = new Date();
+const body = document.querySelector('body'),
+  color = document.querySelector('#color'),
+  button = document.querySelector('#change');
 
-  let element = document.querySelector(".time2");
-  element.innerHTML = now.toLocaleString("ru") ;
-}
+const changeColor = () => {
+  let randomColor =`#${Math.random().toString(16).slice(2,8)}` ;
+  body.style.background = randomColor;
+  color.textContent = randomColor;
+  button.style.color = randomColor;
+};
 
-setInterval(getFormatA, 1000);
-setInterval(getFormatB, 1000);
+button.addEventListener('click', changeColor);
