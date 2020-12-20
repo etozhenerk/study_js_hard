7dc21f297b1b2aca6649739565e276f0d1eb8817
etozhenerk <etozhenerk@yandex.ru>
2020-12-20 18:29:26 +0300

    Второе задание

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
-"use strict";
+'use strict';
 
-const input = document.querySelector("input"),
-  p = document.querySelector("p");
+const box  = document.querySelector('.box'),
+  start = document.querySelector('.start'),
+  reset = document.querySelector('.reset');
 
-function debounce(callBack, time) {
-  return function () {
-    let previousCall = this.lastCall;
-    this.lastCall = Date.now();
-    if (previousCall && this.lastCall - previousCall <= time) {
-      clearTimeout(this.lastCallTimer);
-    }
-    this.lastCallTimer = setTimeout(() => callBack(), time);
-  };
-}
-
-let addText = () => {
-  p.textContent = input.value;
+let count = 0,
+interval;
+const getAnimate = () =>{
+  interval = requestAnimationFrame(getAnimate);
+  count++;
+  if(count < 350){
+    box.style.left = count * 2 + 'px';
+  }
 };
+let animate = false;
 
-input.addEventListener("input", debounce(addText, 300));
+start.addEventListener('click', () => {
+  if(!animate){
+    animate = true;
+    interval = requestAnimationFrame(getAnimate);
+  }else{
+    animate = false;
+    cancelAnimationFrame(interval);
+  }
+    
+});
+
+reset.addEventListener('click', () =>{
+  cancelAnimationFrame(interval);
+  count = 0;
+  box.style.left = 0 + 'px';
+})
